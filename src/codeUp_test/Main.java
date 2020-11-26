5dc3d2d0698ee4fbf3b0b947ae50fa59bb2809d5
cjy324 <juy32400@gmail.com>
2020-11-26 13:46:26 +0900

    1038 : [기초-산술연산] 정수 2개 입력받아 합 출력하기1(설명)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -8,50 +8,14 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		int size = sc.nextInt();
-		int select = sc.nextInt();
-
-		Gas[] gases = new Gas[size];
-
-		for (int i = 0; i < size; i++) {
-			Gas gas = new Gas();
-			gas.name = sc.next();
-			gas.score = sc.nextInt();
-
-			gases[i] = gas;
-		}
-
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		
+		System.out.println(a+b);
+		
 		sc.close();
 
-		System.out.println();
-
-		Gas gas;
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size - i - 1; j++) {
-				if (gases[j].score > gases[j + 1].score) {
-					gas = gases[j];
-					gases[j] = gases[j + 1];
-					gases[j + 1] = gas;
-				}
-			}
-		}
-
-
-
-		for (int i = select; i > 0; i--) {
-
-			gases[i].정답();
-		}
-
 	}
 
 }
 
-class Gas {
-	public String name;
-	public int score;
-
-	void 정답() {
-		System.out.printf("%s\n", name);
-	}
-}
