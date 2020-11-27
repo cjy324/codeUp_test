ed5a355bb29c0aacb6b3216737e63c5925ec5f26
cjy324 <juy32400@naver.com>
2020-11-27 18:05:25 +0900

    1062 : [기초-비트단위논리연산] 비트단위로 XOR 하여 출력하기(설명)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -11,12 +11,9 @@ public class Main {
 		long a = sc.nextLong();
 		long b = sc.nextLong();
 
-		if(a!=1 && b!=1) {
-			System.out.println(1);
-		}
-		else {
-			System.out.println(0);
-		}
+		
+			System.out.println(a^b);
+		
 		
 		
 		sc.close();
