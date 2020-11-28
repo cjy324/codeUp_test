87347cb4968aec2e2f49035dca7d98a498c18a28
cjy324 <juy32400@naver.com>
2020-11-28 17:03:13 +0900

    1110 : 정수 그대로 출력하기

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -8,16 +8,9 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		long a = sc.nextLong();
-		long b = sc.nextLong();
-		long c = sc.nextLong();
-
+		int a = sc.nextInt();
 		
-	//크기가 더 큰 타입을 작은 타입으로 바꿀 때는 명시적으로 변환해주어야 함
-		float f =(float)(((a*b*c)/8)/1024)/1024;
-		   
-		
-		System.out.printf("%.2f MB",f);
+		System.out.printf("%d",a);
 		
 		
 		
