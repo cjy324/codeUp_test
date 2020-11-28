46c52516676afbd1774e3be58c4d37ca8bc4a5f3
cjy324 <juy32400@naver.com>
2020-11-28 16:34:37 +0900

    1085 : [기초-종합] 소리 파일 저장용량 계산하기(설명)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -10,9 +10,12 @@ public class Main {
 
 		long a = sc.nextLong();
 		long b = sc.nextLong();
-
+		long c = sc.nextLong();
+		long d = sc.nextLong();
+		float f =(float)(((a*b*c*d)/8)/1024)/1024;
+		   //크기가 더 큰 타입을 작은 타입으로 바꿀 때는 명시적으로 변환해주어야 함
 		
-			System.out.println(a^b);
+		System.out.printf("%.1f MB",f);
 		
 		
 		
