8137040e92b417462a47cc44468f57b5ab02d5b4
cjy324 <juy32400@naver.com>
2020-11-28 16:58:08 +0900

    1086 : [기초-종합] 그림 파일 저장용량 계산하기(설명)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -11,11 +11,13 @@ public class Main {
 		long a = sc.nextLong();
 		long b = sc.nextLong();
 		long c = sc.nextLong();
-		long d = sc.nextLong();
-		float f =(float)(((a*b*c*d)/8)/1024)/1024;
-		   //크기가 더 큰 타입을 작은 타입으로 바꿀 때는 명시적으로 변환해주어야 함
+
 		
-		System.out.printf("%.1f MB",f);
+	//크기가 더 큰 타입을 작은 타입으로 바꿀 때는 명시적으로 변환해주어야 함
+		float f =(float)(((a*b*c)/8)/1024)/1024;
+		   
+		
+		System.out.printf("%.2f MB",f);
 		
 		
 		
