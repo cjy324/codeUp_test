007693c4d0045e83ab7ed80947210de78ed34cf3
cjy324 <juy32400@naver.com>
2020-12-01 17:56:29 +0900

    1162 : 당신의 사주를 봐 드립니다 1

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -10,33 +10,17 @@ public class Main {
 
 		long a = sc.nextLong();
 		long b = sc.nextLong();
-		
-		String a1 = "";
-		String b1 = "";
-		
-		if(a%2 == 0) {
-			a1 = "짝수";
-		}
-		else if(a%2 != 0){
-			a1 = "홀수";
-		}
-		if(b%2 == 0) {
-			b1 = "짝수";
-		}
-		else if(b%2 != 0){
-			b1 = "홀수";
-		}
-		if((a1.equals("홀수") && b1.equals("짝수")) || (a1.equals("짝수") && b1.equals("홀수"))){
-			System.out.println(a1+"+"+b1+"="+"홀수");
-		}
-		else{
-			System.out.println(a1+"+"+b1+"="+"짝수");
+		long c = sc.nextLong();
+
+		long d = (a - b + c);
+		if (d % 10 == 0) {
+			System.out.println("대박");
+		} else {
+			System.out.println("그럭저럭");
 		}
 
-				
 		sc.close();
 
 	}
 
 }
-
