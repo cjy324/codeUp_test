f42c52a47f9345b2c0be2237895921dc515feef3
cjy324 <juy32400@gmail.com>
2020-12-01 09:14:06 +0900

    1070 : [기초-조건/선택실행구조] 월 입력받아 계절 출력하기(설명)(switch 활용)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -9,11 +9,30 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-
-		System.out.printf("%d",(a<b ? a:b) < c ? (a<b ? a:b):c);
-
+		
+		switch(a) {
+			case 12:
+			case 1:
+			case 2:
+			 System.out.println("winter");	
+			 break;
+			case 3:
+			case 4:
+			case 5:
+			 System.out.println("spring");
+			 break;
+			case 6:
+			case 7:
+			case 8:
+			 System.out.println("summer");
+			 break;
+			case 9:
+			case 10:
+			case 11:
+			 System.out.println("fall");
+			 break;
+		}
+		
 				
 		sc.close();
 
