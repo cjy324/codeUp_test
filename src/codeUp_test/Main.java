831af2e0e1e294edc3fdecb71e1fbdf8efa5e4d7
cjy324 <juy32400@naver.com>
2020-12-05 14:07:55 +0900

    1165 : 축구의 신 1(정답)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -8,20 +8,22 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		long a = sc.nextLong();
-		long b = sc.nextLong();
-		long c = sc.nextLong();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		
-		if (a > 170 && b > 170 && c > 170) {
-			System.out.println("PASS");
+		b=b+1;  //들어가자마자 득점
+		
+		for(int i = 0; i < 90; i++) {  //90분 전까지 득점 카운트
+			if(i == a+5) { //만약, i값이 들어간 시간에서 5분이 지난값과 같으면
+				b = b + 1;  //1득점 추가
+				a = a+5;    //다음 득점 가능 시간 설정
+			}
+			
+//			System.out.printf("%d,%d,%d\n",i,b,a);
 		}
 		
-		else if(a <= 170 && b <= 170 && c <= 170){
-			System.out.println("CRASH");
-		}
-
-	
-
+		System.out.println(b);
+		
 		sc.close();
 
 	}
