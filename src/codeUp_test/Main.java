e6f37da62774a7a331591d240b1a3ab38356ce69
cjy324 <juy32400@naver.com>
2020-12-08 18:12:22 +0900

    1180 : 만능 휴지통(진행중)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -8,23 +8,10 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = b - 30;
-		int d = 0;
+		String a = sc.next();
+		String b = a.substring(0, 1);
 		
-		if(c < 0) {
-			d = a - 1;
-			if(d < 0) {
-				d=d+24;
-			}
-			c = c+60;
-			System.out.printf("%d %d",d ,c);
-		}
-		else if(c >= 0) {
-			d = a;
-			System.out.printf("%d %d",d ,c);
-		}
+
 		
 		sc.close();
 
