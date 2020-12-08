0f0c294590905ee1ef17ca12c06ebbc1bb53ddc7
cjy324 <juy32400@gmail.com>
2020-12-08 09:14:46 +0900

    1167 : 두 번째로 작은 수

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -9,17 +9,26 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int a = sc.nextInt();
-//		int b = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
 		
-		if(a%4 == 0 && a%100 != 0) {
-			System.out.println("yes");
+		if(a >= b && a >= c && b >= c) {
+			System.out.println(b);
 		}
-		else if(a%400 == 0) {
-			System.out.println("yes");
+		else if(a >= b && a >= c && b <= c) {
+			System.out.println(c);
 		}
-		else {
-			System.out.println("no");
-
+		else if(b >= a && b >= c && a >= c) {
+			System.out.println(a);
+		}
+		else if(b >= a && b >= c && a <= c) {
+			System.out.println(c);
+		}
+		else if(c >= a && c >= b && a >= b) {
+			System.out.println(a);
+		}
+		else if(c >= a && c >= b && a <= b) {
+			System.out.println(b);
 		}
 		
 		sc.close();
