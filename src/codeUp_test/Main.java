bcc0fcd187fccf90ebe42daf90bce6f2577c99da
cjy324 <juy32400@gmail.com>
2020-12-09 09:32:47 +0900

    1202 : 등급 판정

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -8,28 +8,25 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		String a = sc.next();
-		String b = a.substring(0,1);    //문자열 자르기
-		String c = a.substring(1,2);
-		int d = Integer.parseInt(c.concat(b));    //문자열 붙이기
-		
-		//만약 이 알고리즘의 심각한 부작용으로 수치가 100이 넘는다면 100의 자릿수는 무시된다.
+		int a = sc.nextInt();
 
-		
-		
-		if(d*2 <= 50) {
-			System.out.println(d*2);
-			System.out.println("GOOD");	
+		if(a >= 90) {
+			System.out.println("A");
 		}
-		else if(d*2 > 100) {
-			System.out.println((d*2)-100);
-			if((d*2)-100 > 50) {
-				System.out.println("OH MY GOD");	
-			}
-			else if((d*2)-100 <= 50) {
-				System.out.println("GOOD");	
-			}
+		if(a < 90 && a >=80) {
+			System.out.println("B");
 		}
+		if(a < 80 && a >=70) {
+			System.out.println("C");
+		}
+		if(a < 70 && a >=60) {
+			System.out.println("D");
+		}
+		if(a < 60 ) {
+			System.out.println("F");
+		}
+
+
 
 		
 		
