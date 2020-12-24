9632cc359468cccdd9b7ba6f35e173dfa8537785
cjy324 <juy32400@naver.com>
2020-12-24 15:06:23 +0900

    1206 : 배수

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -8,27 +8,20 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		float a = sc.nextFloat();
-		float b = sc.nextFloat();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = 0;
 		
-		float p = a+b;
-		float m1 = a-b;
-		float m2 = b-a;
-		float mu = a*b;
-		float d1 = a/b;
-		float d2 = b/a;
-		float s1 = (float) Math.pow(a, b);
-		float s2 = (float) Math.pow(b, a);
-		
-		
-		
-		float[] array = new float[]{p, m1, m2, mu, d1, d2, s1, s2};
-		for(int i = 0; i < array.length; i++) {
-			float max = array[i];
-			if(array[i] < array[i+1]) {
-				max = array[i+1];
-			}
-			System.out.printf("%.6f",max);
+		if(b%a == 0 && b >= a) {
+			c = b/a;
+			System.out.println(a + "*" + c + "=" + b);
+		}
+		if(a%b == 0 && b < a) {
+			c = a/b;
+			System.out.println(b + "*" + c + "=" + a);
+		}
+		else if(b%a != 0 && a%b != 0){
+			System.out.println("none");
 		}
 		
 
