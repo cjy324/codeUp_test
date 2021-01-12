741c3cd21d614454f19994d8962b5bb6b4961ccf
cjy324 <juy32400@naver.com>
2021-01-12 13:59:52 +0900

    **1218 : 삼각형 판단하기(더 쉬운 정답)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -12,74 +12,17 @@ public class Main {
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 
-
-		//일반 삼각형 성립 조건
-		if (a >= b && a >= c) {
-			if (a < (b + c)) {
-				//정삼각형
-				if(a == b && b == c && a == c){
-					System.out.println("정삼각형");
-				}
-				//이등변삼각형
-				else if(((a == b) && a != c) || ((a == c) && b != c) || ((b == c) && a != c)) {
-					System.out.println("이등변삼각형");
-				}
-				//직각삼각형
-				else if(((a*a) + (b*b)) == (c*c) || ((a*a) + (c*c)) == (b*b) || ((c*c) + (b*b)) == (a*a)) {
-					System.out.println("직각삼각형");
-				}
-				else {
-				System.out.println("삼각형");
-				}
-			}
-			else {
-				System.out.println("삼각형아님");
-			}
-		} else if (b >= a && b >= c) {
-			if (b < (a + c)) {
-				//정삼각형
-				if(a == b && b == c && a == c){
-					System.out.println("정삼각형");
-				}
-				//이등변삼각형
-				else if(((a == b) && a != c) || ((a == c) && b != c) || ((b == c) && a != c)) {
-					System.out.println("이등변삼각형");
-				}
-				//직각삼각형
-				else if(((a*a) + (b*b)) == (c*c) || ((a*a) + (c*c)) == (b*b) || ((c*c) + (b*b)) == (a*a)) {
-					System.out.println("직각삼각형");
-				}
-				else {
-				System.out.println("삼각형");
-				}
-			}
-			else {
-				System.out.println("삼각형아님");
-			}
-		} else if (c >= a && c >= b) {
-			if (c < (a + b)) {
-				//정삼각형
-				if(a == b && b == c && a == c){
-					System.out.println("정삼각형");
-				}
-				//이등변삼각형
-				else if(((a == b) && a != c) || ((a == c) && b != c) || ((b == c) && a != c)) {
-					System.out.println("이등변삼각형");
-				}
-				//직각삼각형
-				else if(((a*a) + (b*b)) == (c*c) || ((a*a) + (c*c)) == (b*b) || ((c*c) + (b*b)) == (a*a)) {
-					System.out.println("직각삼각형");
-				}
-				else {
-				System.out.println("삼각형");
-				}
-			}
-			else {
-				System.out.println("삼각형아님");
-			}
+		if (a + b <= c) {
+			System.out.println("삼각형아님");
+		} else if (a == b && b == c) {
+			System.out.println("정삼각형");
+		} else if (a == b || b == c) {
+			System.out.println("이등변삼각형");
+		} else if ((a * a) + (b * b) == (c * c)) {
+			System.out.println("직각삼각형");
+		} else {
+			System.out.println("삼각형");
 		}
-
-		
 
 		sc.close();
 
