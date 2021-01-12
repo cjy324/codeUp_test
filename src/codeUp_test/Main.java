c3b2622d1afad145f13a0ad6b899f92bcf8fe76d
cjy324 <juy32400@naver.com>
2021-01-12 13:49:38 +0900

    **1218 : 삼각형 판단하기(삼각형 공식 다수)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -8,20 +8,79 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		int notAd = sc.nextInt(); //1
-		int doAd = sc.nextInt(); //3
-		int payAd = sc.nextInt(); //1
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+
+		//일반 삼각형 성립 조건
+		if (a >= b && a >= c) {
+			if (a < (b + c)) {
+				//정삼각형
+				if(a == b && b == c && a == c){
+					System.out.println("정삼각형");
+				}
+				//이등변삼각형
+				else if(((a == b) && a != c) || ((a == c) && b != c) || ((b == c) && a != c)) {
+					System.out.println("이등변삼각형");
+				}
+				//직각삼각형
+				else if(((a*a) + (b*b)) == (c*c) || ((a*a) + (c*c)) == (b*b) || ((c*c) + (b*b)) == (a*a)) {
+					System.out.println("직각삼각형");
+				}
+				else {
+				System.out.println("삼각형");
+				}
+			}
+			else {
+				System.out.println("삼각형아님");
+			}
+		} else if (b >= a && b >= c) {
+			if (b < (a + c)) {
+				//정삼각형
+				if(a == b && b == c && a == c){
+					System.out.println("정삼각형");
+				}
+				//이등변삼각형
+				else if(((a == b) && a != c) || ((a == c) && b != c) || ((b == c) && a != c)) {
+					System.out.println("이등변삼각형");
+				}
+				//직각삼각형
+				else if(((a*a) + (b*b)) == (c*c) || ((a*a) + (c*c)) == (b*b) || ((c*c) + (b*b)) == (a*a)) {
+					System.out.println("직각삼각형");
+				}
+				else {
+				System.out.println("삼각형");
+				}
+			}
+			else {
+				System.out.println("삼각형아님");
+			}
+		} else if (c >= a && c >= b) {
+			if (c < (a + b)) {
+				//정삼각형
+				if(a == b && b == c && a == c){
+					System.out.println("정삼각형");
+				}
+				//이등변삼각형
+				else if(((a == b) && a != c) || ((a == c) && b != c) || ((b == c) && a != c)) {
+					System.out.println("이등변삼각형");
+				}
+				//직각삼각형
+				else if(((a*a) + (b*b)) == (c*c) || ((a*a) + (c*c)) == (b*b) || ((c*c) + (b*b)) == (a*a)) {
+					System.out.println("직각삼각형");
+				}
+				else {
+				System.out.println("삼각형");
+				}
+			}
+			else {
+				System.out.println("삼각형아님");
+			}
+		}
+
 		
-		if(notAd > (doAd-payAd)) {
-			System.out.println("do not advertise");
-		}
-		if((doAd-payAd) > notAd) {
-			System.out.println("advertise");
-		}
-		if((doAd-payAd) == notAd) {
-			System.out.println("does not matter");
-		}
-		
+
 		sc.close();
 
 	}
