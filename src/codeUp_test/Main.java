8bbc8f401762b2d4ff1dd2ac31c0ef73e7deb984
cjy324 <juy32400@naver.com>
2021-01-12 11:02:06 +0900

    1207: 윷놀이

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -12,10 +12,25 @@ public class Main {
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		int d = sc.nextInt();
-			
-			
 		
+		int abcd = a+b+c+d;
 		
+		if(abcd == 1) {
+			System.out.println("도");
+		}
+		else if(abcd == 2) {
+			System.out.println("개");
+		}
+		else if(abcd == 3) {
+			System.out.println("걸");
+		}
+		else if(abcd == 4) {
+			System.out.println("윷");
+		}
+		else if(abcd == 0) {
+			System.out.println("모");
+		}
+
 		sc.close();
 
 	}
