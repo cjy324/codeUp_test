8ad58b02da59f3337f90eee0bc3e96b9b08f8053
cjy324 <juy32400@naver.com>
2021-03-18 22:43:21 +0900

    1090 : [기초-종합] 수 나열하기2(****)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -8,16 +8,20 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		Long a = sc.nextLong();
+		Long b = sc.nextLong();
 		int c = sc.nextInt();
 		
-		Integer[] I = new Integer[c+1];
+		Long[] I = new Long[c+1];
 		
 		for(int i = 0; i < I.length-1; i++) {
 			I[0] = a;
-			I[i+1] = I[i] + b;
+			I[i+1] = I[i]*b;
 		}
+		//int형으로 하면 담을 수 있는 범위를 초과해버림
+		//따라서, int보다 큰 범위까지 커버가 가능한
+		//long형으로 배열을 만들어야 함
+		
 		System.out.println(I[c-1]);
 		
 	
