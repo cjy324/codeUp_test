15595433f452ff61c465190dcad14202e781031d
cjy324 <juy32400@naver.com>
2021-05-05 16:48:10 +0900

    21.05.05 코딩테스트 - 폰켓몬 정답&해설

--- src/algorithm_test/Main.java
+++ src/algorithm_test/Main.java
@@ -1,25 +1,34 @@
 package algorithm_test;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashSet;
 
 public class Main {
 	public static void main(String[] args) {
-	}		
+	}
 }
 
-// 내가 푼 방식
+//내가 푼 정답
 class Solution {
-    public String solution(int[] numbers) {
-        String answer = "";
-        Integer[] nums = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
-       
-        //1. 배열 값들을 오름차순으로 정렬
-        Arrays.sort(nums, Collections.reverseOrder());        
-        for(int i = 0; i < nums.length; i++){
-            answer += Integer.toString(nums[i]);
+    public int solution(int[] nums) {
+        int answer = 0;
+        int count = nums.length/2;
+   
+        HashSet<Integer> newNums = new HashSet<>();
+        
+        //1.배열 내 중복 제거 후 HashSet으로 담기
+        for(int i = 0; i < nums.length; i++) {
+        	newNums.add(nums[i]);
         }
-        //3. String으로 붙이기
+        
+        //2.1번 배열의 길이 = 종류 최댓값
+        //예외) 최대 선택 가능한 값을 초과할 순 없음
+        if(newNums.size() > count) {
+        	answer = count;
+        }
+        else {
+        	answer = newNums.size();
+        }
+        
         return answer;
     }
 }
