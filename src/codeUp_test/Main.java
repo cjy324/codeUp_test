1b9ba91bea7a49d0339dcf7affa54d88620521ce
cjy324 <juy32400@naver.com>
2021-05-11 22:29:57 +0900

    21.05.11 100제 다시풀기 1025번까지 완료

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -5,28 +5,22 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
+		
+		String x = sc.next();
+		String y1 = x.substring(0,1);
+		String y2 = x.substring(1,2);
+		String y3 = x.substring(2,3);
+		String y4 = x.substring(3,4);
+		String y5 = x.substring(4);
+		
+		System.out.printf("[%s0000]\n", y1);
+		System.out.printf("[%s000]\n", y2);
+		System.out.printf("[%s00]\n", y3);
+		System.out.printf("[%s0]\n", y4);
+		System.out.printf("[%s]\n", y5);
 
-		Long a = sc.nextLong();
-		Long b = sc.nextLong();
-		int c = sc.nextInt();
-		
-		Long[] I = new Long[c+1];
-		
-		for(int i = 0; i < I.length-1; i++) {
-			I[0] = a;
-			I[i+1] = I[i]*b;
-		}
-		//int형으로 하면 담을 수 있는 범위를 초과해버림
-		//따라서, int보다 큰 범위까지 커버가 가능한
-		//long형으로 배열을 만들어야 함
-		
-		System.out.println(I[c-1]);
-		
-	
+
 		sc.close();
-
 	}
-
 }
