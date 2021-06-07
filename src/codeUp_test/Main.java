9e8a92c5efd8d5b99d5f38fbf61fc5771efdfd23
cjy324 <juy32400@naver.com>
2021-06-07 20:01:57 +0900

    21.06.07 백준 - 정수 N개의 합(정답)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -1,23 +1,13 @@
 package codeUp_test;
 
-import java.util.*;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-	
-		int a = sc.nextInt();
-		int m = sc.nextInt();
-		int d = sc.nextInt();
-		int n = sc.nextInt();
-		long x = a;
-		
-		for(int i = 1; i < n; i++) {
-			x = (x*m)+d;
-		}
-		System.out.println(x);
+	}
+}
 
-		sc.close();
+class Test {
+	long sum(int[] a) {
+		return 0;
 	}
 }
