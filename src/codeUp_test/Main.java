7829ab26f1365e973f59207a1814bc737f537256
cjy324 <juy32400@naver.com>
2021-06-07 21:35:42 +0900

    21.06.07 숫자형 기본 타입별 크기

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -1,19 +1,35 @@
 package codeUp_test;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-	}
-}
-
-class Test {
-	long sum(int[] a) {
+		Scanner sc = new Scanner(System.in);
 		
-		long x = 0;
+		System.out.println("기본 숫자형 타입의 최대/최소값");
 		
-		for(int i = 0; i < a.length; i++) {
-			x += a[i];
-		}
-		return x;
+		System.out.println("--- 정수 타입 ---");
+		System.out.println("byte(1바이트)");
+		System.out.println("최대값: " + Byte.MAX_VALUE);
+		System.out.println("최소값: " + Byte.MIN_VALUE);
+		System.out.println("short(2바이트)");
+		System.out.println("최대값: " + Short.MAX_VALUE);
+		System.out.println("최소값: " + Short.MIN_VALUE);
+		System.out.println("int(4바이트)");
+		System.out.println("최대값: " + Integer.MAX_VALUE);
+		System.out.println("최소값: " + Integer.MIN_VALUE);
+		System.out.println("long(8바이트)");
+		System.out.println("최대값: " + Long.MAX_VALUE);
+		System.out.println("최소값: " + Long.MIN_VALUE);
+		
+		System.out.println("--- 실수 타입 ---");
+		System.out.println("float(4바이트)");
+		System.out.println("최대값: " + Float.MAX_VALUE);
+		System.out.println("최소값: " + Float.MIN_VALUE);
+		System.out.println("double(8바이트)");
+		System.out.println("최대값: " + Double.MAX_VALUE);
+		System.out.println("최소값: " + Double.MIN_VALUE);
+		sc.close();
 	}
 }
