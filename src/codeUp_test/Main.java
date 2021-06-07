b0057733cb3f22a6b8a922553226f8993647f864
cjy324 <juy32400@naver.com>
2021-06-07 23:20:10 +0900

    21.06.07 1081 : [기초-종합] 주사위를 2개 던지면?(정답)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -7,29 +7,17 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("기본 숫자형 타입의 최대/최소값");
+		//n,m < 10
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 		
-		System.out.println("--- 정수 타입 ---");
-		System.out.println("byte(1바이트)");
-		System.out.println("최대값: " + Byte.MAX_VALUE);
-		System.out.println("최소값: " + Byte.MIN_VALUE);
-		System.out.println("short(2바이트)");
-		System.out.println("최대값: " + Short.MAX_VALUE);
-		System.out.println("최소값: " + Short.MIN_VALUE);
-		System.out.println("int(4바이트)");
-		System.out.println("최대값: " + Integer.MAX_VALUE);
-		System.out.println("최소값: " + Integer.MIN_VALUE);
-		System.out.println("long(8바이트)");
-		System.out.println("최대값: " + Long.MAX_VALUE);
-		System.out.println("최소값: " + Long.MIN_VALUE);
+		for(int i = 1; i <= n; i++) {
+			for(int k = 1; k <= m; k++) {
+				System.out.printf("%d %d\n", i,k);
+			}
+		}
+
 		
-		System.out.println("--- 실수 타입 ---");
-		System.out.println("float(4바이트)");
-		System.out.println("최대값: " + Float.MAX_VALUE);
-		System.out.println("최소값: " + Float.MIN_VALUE);
-		System.out.println("double(8바이트)");
-		System.out.println("최대값: " + Double.MAX_VALUE);
-		System.out.println("최소값: " + Double.MIN_VALUE);
 		sc.close();
 	}
 }
