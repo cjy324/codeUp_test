4105715cfdc8f20456ac9779d0814ecfef2873b0
cjy324 <juy32400@naver.com>
2021-06-08 22:51:09 +0900

    21.06.08 1087 : [기초-종합] 여기까지! 이제 그만(정답)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -7,22 +7,19 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int r = sc.nextInt();
-		int g = sc.nextInt();
-		int b = sc.nextInt();
+		int x = sc.nextInt();
 		sc.close();
 		
-		int total = 0;
-		
-		for(int i = 0; i < r; i++) {
-			for(int j = 0; j < g; j++) {
-				for(int k = 0; k < b; k++) {
-					System.out.printf("%d %d %d\n",i,j,k);
-					total++;
-				}
+		int i = 1;
+		int sum = 0;
+		while(true) {
+			sum += i;
+			if(sum >= x) {
+				System.out.println(sum);
+				break;
 			}
+			i++;
 		}
-		System.out.println(total);
 
 	}
 }
