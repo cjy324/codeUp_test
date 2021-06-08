25239158ef9650b2b579072a5e399369bcb5dd63
cjy324 <juy32400@naver.com>
2021-06-08 21:34:09 +0900

    21.06.08 1083 : [기초-종합] 3 6 9 게임의 왕이 되자(정답)

--- src/codeUp_test/Main.java
+++ src/codeUp_test/Main.java
@@ -7,21 +7,16 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int h = sc.nextInt();
-		int m = sc.nextInt();
+		int x = sc.nextInt();
 		sc.close();
 		
-		if(m < 45) {
-			h--;
-			if(h < 0) {
-				h = 23;
+		for(int i = 1; i <= x; i++) {
+			if(i%3 == 0) {
+				System.out.printf("%s ", "X");
 			}
-			m = 60 - (45 - m);
-			System.out.printf("%d %d", h, m);
+			else {
+				System.out.printf("%d ",i);
+			}
 		}
-		else {
-			System.out.printf("%d %d", h, m-45);
-		}
-		
 	}
 }
